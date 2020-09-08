8639d2643fa07c0dacc036b4d4b8e5963dd9c95a
Monica Monteiro <mm110521@grpoesp.com.br>
2020-09-07 22:10:43 -0300

    Recipes - Criando o model Ingrediente - novo construtor

--- recipes/src/app/shared/ingredient.model.ts
+++ recipes/src/app/shared/ingredient.model.ts
@@ -1,12 +1,3 @@
-import { ThrowStmt } from '@angular/compiler';
-import { timeStamp } from 'console';
-
 export class Ingredient {
-  public name: string;
-  public amount: number;
-
-  constructor(name: string, amount: number) {
-    this.name = name;
-    this.amount = amount;
-  }
+  constructor(public name: string, public amount: number) {}
 }
