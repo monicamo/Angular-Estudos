781189f6972e985b57913c084ddb8fd3ce664719
Monica Monteiro <monicamo@gmail.com>
2020-09-16 22:22:00 -0300

    ECMA - ARRAYS

--- ecmascript/src/app/cap01/cap01.component.ts
+++ ecmascript/src/app/cap01/cap01.component.ts
@@ -15,6 +15,51 @@ export class Cap01Component implements OnInit {
     this.onMap();
     this.onFilter();
     this.onFind();
+    this.onEvery();
+    this.onSome();
+    this.onReduce();
+    this.onReduceDois();
+  }
+  onReduceDois() {
+    const pessoas = [
+      {nome: 'Joao', idade: 12},
+      {nome: 'Maria', idade: 18},
+      {nome: 'Jose', idade: 21}
+    ];
+    const nomes = pessoas.reduce((arrayNomes, pessoa) => {
+      arrayNomes.push(pessoa.nome);
+      return arrayNomes;
+    }, []);
+    console.log(nomes);
+  }
+
+  onReduce() {
+    const numeros = [1, 2, 3, 4, 5, 6, 7];
+    let soma = 0;
+    soma = numeros.reduce((soma, numero) => {
+      return soma + numero;
+    });
+    console.log(soma);
+  }
+
+  onSome() {
+    const pesoMalas = [21, 16, 25, 32, 34];
+    const bool = pesoMalas.some(mala => {
+      return mala > 25;
+    });
+    console.log(bool);
+  }
+
+  onEvery() {
+    const pessoas = [
+      {nome: 'Joao', idade: 12},
+      {nome: 'Maria', idade: 18},
+      {nome: 'Jose', idade: 21}
+    ];
+    const bool = pessoas.every(pessoa => {
+      return pessoa.idade >= 18;
+    });
+    console.log("every " + bool);
   }
 
   onFind() {
